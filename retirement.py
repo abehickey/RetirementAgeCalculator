73218fdd293de02f06bd06e3ca0627352f192e5c
Abe Hickey <ahickey@my.waketech.edu>
2019-11-17 20:51:56 -0500

    update to correct file

--- retirement.py
+++ retirement.py
@@ -1,50 +1,57 @@
-# ----------------------------------------------------------------------
-# Imports
-# ----------------------------------------------------------------------
+#####################################################
+# Programmer: Abe Hickey
+# Class: CSC.256.0001
+# File: retirement.py
+# Description: This program calculates when someone will receive full Social Security Benefits
+#####################################################
 
-import calendar
+import retireCalc
 import datetime
 
 
-# ----------------------------------------------------------------------
-# Calculation Functions
-# ----------------------------------------------------------------------
-
-def calculate_retirement_age(birth_year):
-  if birth_year <= 1937:
-    return 65, 0
-  elif birth_year == 1938:
-    return 65, 2
-  elif birth_year == 1939:
-    return 65, 4
-  elif birth_year == 1940:
-    return 65, 6
-  elif birth_year == 1941:
-    return 65, 8
-  elif birth_year == 1942:
-    return 65, 10
-  elif 1943 <= birth_year <= 1954:
-    return 66, 0
-  elif birth_year == 1955:
-    return 66, 2
-  elif birth_year == 1956:
-    return 66, 4
-  elif birth_year == 1957:
-    return 66, 6
-  elif birth_year == 1958:
-    return 66, 8
-  elif birth_year == 1959:
-    return 66, 10
-  else:
-    return 67, 0
+# Get Birth year from user
+def inputYear():
+    while True:
+        try:
+            userInput = int(input("Please enter 4-digit birth year: "))
+            if userInput < 1900 or userInput > int(datetime.datetime.now().year):  # if not 1900 to current year print message and ask for input again
+                print("Sorry, input must be a 4-digit year between 1900 and the current year")
+                continue
+        except ValueError:
+            print("Not an integer! Try again.")
+            continue
+        else:
+            return userInput
 
 
-def calculate_retirement_date(birth_year, birth_month, age_years, age_months):
-  year = birth_year + age_years
-  month = birth_month + age_months
+# Get Birth month from user
+def inputMonth():
+    print("1. January,  2. February,  3. March,  4. April")
+    print("5. May,  6. June,  7. July,  8. August")
+    print("9. September,  10. October, 11. November, 12. December")
+    while True:
+        try:
+            userInput = int(input("Please the number of the birth month: "))
+            if userInput < 1 or userInput > 12:  # if not from 1 to 12 print message and ask for input again
+                print("Sorry, input must be a number from 1 to 12.")
+                continue
+            # break
+        except ValueError:
+            print("Not an integer! Try again.")
+            continue
+        else:
+            return userInput
 
-  if month > 12:
-    year += 1
-    month -= 12
-  
-  return year, month
+
+birthYear = inputYear()
+birthMon = inputMonth()
+print()
+
+age, mon = retireCalc.getAge(birthYear) #Get retirement age and month
+if mon == 0:
+    print("Age for obtaining full SSA benefits: ", age)
+else:
+    print("Age for obtaining full SSA benefits: ", age, "and", mon, "months")
+
+ssaYear, ssaMonth = retireCalc.getWhen(birthYear,birthMon,age,mon) #Determine when benefits can be obtained
+print("Full SSA benefits may be obtained " + ssaMonth + " " + str(ssaYear) + ".")
